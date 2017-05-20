a1a57e568a34521e969e428cd6468207bdea0bf1
Martin Woelke <bgl.boardgameleague@gmail.com>
2017-05-20 13:13:24 +0200

    Added some old attempt working with a dataset. Let's experiment a bit with it...

---
+++ BoardGameLeageLibDataSet/Helpers/VersionWrapper.cs
@@ -0,0 +1,147 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace BoardGameLeagueLib.Helpers
+{
+    /// <summary>
+    /// Version class
+    /// used to read verison number from assembly
+    /// </summary>
+    public class VersionWrapper
+    {
+        /// <summary>
+        /// static Get Accessor VersionName
+        /// </summary>
+        public static string NameExecuting
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Name;
+            }
+        }
+
+        /// <summary>
+        /// static Get Accessor VersionName
+        /// </summary>
+        public static string NameCalling
+        {
+            get
+            {
+                return Assembly.GetCallingAssembly().GetName().Name;
+            }
+        }
+
+        /// <summary>
+        /// static Get Accessor VersionName
+        /// </summary>
+        public static string NameVersionExecuting
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Name + " " + VersionNumberExecuting;
+            }
+        }
+
+        /// <summary>
+        /// static Get Accessor VersionName
+        /// </summary>
+        public static string NameVersionCalling
+        {
+            get
+            {
+                string v_Version = Assembly.GetCallingAssembly().GetName().Version.ToString();
+                int v_Index = v_Version.LastIndexOf(".");
+
+                if (v_Index > 0)
+                {
+                    v_Version = v_Version.Substring(0, v_Index);
+                }
+
+                return Assembly.GetCallingAssembly().GetName().Name + " " + v_Version;
+            }
+        }
+
+        /// <summary>
+        /// static Get Accesssor VersionNumber
+        /// </summary>
+        public static string VersionNumberExecuting
+        {
+            get
+            {
+                string v_Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                int v_Index = v_Version.LastIndexOf(".");
+
+                if (v_Index > 0)
+                {
+                    v_Version = v_Version.Substring(0, v_Index);
+                }
+
+                return v_Version;
+            }
+        }
+
+        /// <summary>
+        /// static Get Accesssor VersionNumber
+        /// </summary>
+        public static string VersionNumberCalling
+        {
+            get
+            {
+                string v_Version = Assembly.GetCallingAssembly().GetName().Version.ToString();
+                int v_Index = v_Version.LastIndexOf(".");
+
+                if (v_Index > 0)
+                {
+                    v_Version = v_Version.Substring(0, v_Index);
+                }
+
+                return v_Version;
+            }
+        }
+
+        /// <summary>
+        /// static Get Accessor VersionComplete
+        /// </summary>
+        public static string VersionCompleteExecuting
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        /// <summary>
+        /// static Get Accessor VersionComplete
+        /// </summary>
+        public static string VersionCompleteCalling
+        {
+            get
+            {
+                return Assembly.GetCallingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        private static string GetAssemblyCompany(Assembly a_Assembly)
+        {
+            return FileVersionInfo.GetVersionInfo(a_Assembly.Location).CompanyName;
+        }
+
+        public static string CompanyCalling
+        {
+            get
+            {
+                return GetAssemblyCompany(Assembly.GetCallingAssembly());
+            }
+        }
+
+        public static string CompanyExecuting
+        {
+            get
+            {
+                return GetAssemblyCompany(Assembly.GetExecutingAssembly());
+            }
+        }
+    }
+}
+
+
---
+++ BoardGameLeagueDataSetUI/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using log4net;
+using log4net.Config;
+
+namespace BoardGameLeagueDataSetUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ILog m_Logger;
+        DataSet m_BGLDB = new DataSet();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            Thread.CurrentThread.Name = "UI";
+            XmlConfigurator.Configure(new FileInfo("log4netConfig.xml"));
+            m_Logger = LogManager.GetLogger("MainWindow");
+            m_Logger.Debug("Window starts loading.");
+
+            DataSet v_BGLDB = new DataSet();
+            m_BGLDB.ReadXml("db" + System.IO.Path.DirectorySeparatorChar + "bgldb.xml");
+
+            lbGames.DataContext = m_BGLDB.Tables["Game"];
+            cbGameFamily.DataContext = m_BGLDB.Tables["GameFamily"];
+
+            lbPlayers.DataContext = m_BGLDB.Tables["Player"];
+
+            comboBoxGender.Items.Add("Male");
+            comboBoxGender.Items.Add("Female");
+
+            cbGameType.Items.Add("VictoryPoints");
+            cbGameType.Items.Add("Rank");
+
+            lbGameFamilies.DataContext = m_BGLDB.Tables["GameFamily"];
+            lbLocations.DataContext = m_BGLDB.Tables["Location"];
+
+            //textBoxGameName.DataContext=
+
+        }
+
+        private void AddRow(String a_Tablename)
+        {
+            if (m_BGLDB.Tables.Contains(a_Tablename))
+            {
+                DataRow dr = m_BGLDB.Tables[a_Tablename].NewRow();
+                dr["Id"] = new Guid();
+                dr["Name"] = "blah";
+                dr["Description"] = "blah";
+                m_BGLDB.Tables[a_Tablename].Rows.Add(dr);
+
+            }
+        }
+
+        private void buttonNewLocation_Click(object sender, RoutedEventArgs e)
+        {
+            AddRow("Location");
+        }
+
+    }
+}
