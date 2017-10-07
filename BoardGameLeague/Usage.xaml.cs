0fc49da13084c02d0c42b06b07a7ababea1a202b
Martin Woelke <bgl.boardgameleague@gmail.com>
2017-10-07 16:03:48 +0200

    Rewrote the Usage window so that it loads a embedded RTF. The WPF control is much to difficult to use to just write some help for this tool.

--- BoardGameLeague/Usage.xaml.cs
+++ BoardGameLeague/Usage.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
 
 namespace BoardGameLeagueUI
 {
@@ -10,6 +16,28 @@ namespace BoardGameLeagueUI
         public Usage()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Assembly v_Assembly = Assembly.GetExecutingAssembly();
+            List<String> v_EmbeddedResourceNames = new List<string>(v_Assembly.GetManifestResourceNames());
+            int v_IndexInAssembly = v_EmbeddedResourceNames.FindIndex(x => x.Contains("Usage.rtf"));
+            String v_PathToReadFrom = v_EmbeddedResourceNames[v_IndexInAssembly];
+            TextRange range = new TextRange(rtb.Document.ContentStart, rtb.Document.ContentEnd);
+            range.Load(v_Assembly.GetManifestResourceStream(v_PathToReadFrom), System.Windows.DataFormats.Rtf);
+        }
+
+        /// <summary>
+        /// This helper lets us click links in the loaded RTF and open the it in the default browser. Found the solution here:
+        /// https://stackoverflow.com/questions/762271/clicking-hyperlinks-in-a-richtextbox-without-holding-down-ctrl-wpf
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Hyperlink_MouseLeftButtonDown(object sender, MouseEventArgs e)
+        {
+            var hyperlink = (Hyperlink)sender;
+            Process.Start(hyperlink.NavigateUri.ToString());
         }
     }
 }
