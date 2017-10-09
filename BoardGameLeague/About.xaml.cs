fdb03aa204cdd2e35d96f16d269586a9d8e3e80f
Martin Woelke <bgl.boardgameleague@gmail.com>
2017-10-09 22:37:58 +0200

    Added clickable links and the mail address for the project to the Author window.

--- BoardGameLeague/About.xaml.cs
+++ BoardGameLeague/About.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using BoardGameLeagueLib.Helpers;
 using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Documents;
+using System.Windows.Navigation;
 
 namespace BoardGameLeagueUI
 {
@@ -17,14 +19,34 @@ namespace BoardGameLeagueUI
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            var paragraph = new Paragraph();
-            string v_BuildDateTime = BoardGameLeagueUI.Properties.Resources.BuildDate;
-            paragraph.Inlines.Add(new Run(string.Format(VersionWrapper.NameVersionCalling + Environment.NewLine)));
-            paragraph.Inlines.Add(new Run("Build Date: " + v_BuildDateTime + Environment.NewLine));
-            paragraph.Inlines.Add(new Run("Author: Martin Woelke" + Environment.NewLine));
-            paragraph.Inlines.Add(new Run(Environment.NewLine+ "Icon by mattahan from the Buuf icon set https://mattahan.deviantart.com/art/Buuf-37966044."));
+            Paragraph para = new Paragraph();
 
-            rtbAbout.Document.Blocks.Add(paragraph);
+            para.Inlines.Add(new Run(VersionWrapper.NameVersionCalling + Environment.NewLine));
+            para.Inlines.Add(new Run("Build Date: " + BoardGameLeagueUI.Properties.Resources.BuildDate + Environment.NewLine));
+            para.Inlines.Add(new Run("Write the author: "));
+
+            Hyperlink v_LinkToMail = new Hyperlink(new Run("Martin Woelke"));
+            v_LinkToMail.NavigateUri = new Uri("mailto:bgl.boardgameleague@gmail.com");
+            v_LinkToMail.RequestNavigate += Hyperlink_RequestNavigate;
+            para.Inlines.Add(v_LinkToMail);
+            para.Inlines.Add(" (bgl.boardgameleague@gmail.com)" + Environment.NewLine + Environment.NewLine);
+
+            para.Inlines.Add(new Run("Icon by mattahan from the "));
+            Run v_LinkText = new Run("Buuf icon set.");
+            Hyperlink v_LinkToIconSet = new Hyperlink();
+            v_LinkToIconSet.IsEnabled = true;
+            v_LinkToIconSet.Inlines.Add("Buuf icon set.");
+            v_LinkToIconSet.NavigateUri = new Uri("https://mattahan.deviantart.com/art/Buuf-37966044");
+            v_LinkToIconSet.RequestNavigate += Hyperlink_RequestNavigate;
+            para.Inlines.Add(v_LinkToIconSet);
+
+            rtb.Document.Blocks.Add(para);
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri.ToString()));
+            e.Handled = true;
         }
     }
 }
