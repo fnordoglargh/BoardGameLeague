7b9f096ce23decd1d802db69e836446c822bb9e6
Martin Woelke <bgl.boardgameleague@gmail.com>
2018-01-27 12:08:26 +0100

    Removing trailing whitespaces prevents the DataGrid to mess up its binding.

--- BoardGameLeague/BoardGameLeagueLib/DbClasses/DbObjectName.cs
+++ BoardGameLeague/BoardGameLeagueLib/DbClasses/DbObjectName.cs
@@ -6,12 +6,16 @@ namespace BoardGameLeagueLib.DbClasses
     {
         private String m_Name;
 
+        /// <summary>
+        /// Gets/sets the Name of the DbObject. All trailing whitespaces will be trimmed.
+        /// </summary>
         public String Name
         {
             get { return m_Name; }
             set
             {
-                m_Name = value;
+                // DataGrid Columns will mess up the binding if therfe are thrailing whitepaces.
+                m_Name = value.TrimEnd(' ');
                 NotifyPropertyChanged("Name");
             }
         }
