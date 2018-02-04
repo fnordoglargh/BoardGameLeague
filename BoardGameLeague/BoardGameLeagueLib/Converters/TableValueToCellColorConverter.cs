61ec48011e920c037eb5c26bb49b212805e94ce9
Martin Woelke <bgl.boardgameleague@gmail.com>
2018-02-04 23:16:24 +0100

    Moved TableValueToCellColorConverter to its own class.

---
+++ BoardGameLeague/BoardGameLeagueLib/Converters/TableValueToCellColorConverter.cs
@@ -0,0 +1,48 @@
+﻿using BoardGameLeagueLib.DbClasses;
+using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BoardGameLeagueUI.BoardGameLeagueLib.Converters
+{
+    public class TableValueToCellColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            DataGridCell v_Cell = values[0] as DataGridCell;
+
+            if (v_Cell == null) return Brushes.Transparent;
+
+            int v_RowIndex = CellIndexer.Instance.RowIndex;
+
+            ObservableCollection<Property> v_Row = values[1] as ObservableCollection<Property>;
+
+            if (v_Row == null) return Brushes.Transparent;
+
+            int v_ActualCellValue = -1;
+
+            if (int.TryParse(v_Row[v_RowIndex].Value.ToString(), out v_ActualCellValue))
+            {
+                if (v_ActualCellValue == 0)
+                {
+                    return Brushes.Transparent;
+                }
+                else
+                {
+                    return Brushes.LightSalmon;
+                }
+            }
+            else
+            {
+                return Brushes.Transparent;
+            }
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BoardGameLeague/MainWindow.xaml.cs
+++ BoardGameLeague/MainWindow.xaml.cs
@@ -1640,42 +1640,4 @@ namespace BoardGameLeagueUI
             throw new NotImplementedException();
         }
     }
-
-    public class TableValueToCellColorConverter : IMultiValueConverter
-    {
-        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-        {
-            DataGridCell v_Cell = values[0] as DataGridCell;
-
-            if (v_Cell == null) return Brushes.Transparent;
-
-            int v_RowIndex = CellIndexer.Instance.RowIndex;
-
-            ObservableCollection<Property> v_Row = values[1] as ObservableCollection<Property>;
-
-            if (v_Row == null) return Brushes.Transparent;
-
-            int v_ActualCellValue = -1;
-
-            if(int.TryParse(v_Row[v_RowIndex].Value.ToString(), out v_ActualCellValue))
-            {
-                if (v_ActualCellValue == 0)
-                {
-                    return Brushes.Transparent;
-                }
-                else
-                {
-                    return Brushes.LightSalmon;
-                }
-            }
-            else
-            {
-                return Brushes.Transparent;
-            }
-        }
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
